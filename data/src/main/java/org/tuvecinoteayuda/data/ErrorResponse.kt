1e1e3a62c3d53d8d53aaab3a293d3e1fdb2074f1
Roberto Miranda Pérez <rmp0046@gmail.com>
2020-03-21 18:03:40 -0300

    update Error Response

--- data/src/main/java/org/tuvecinoteayuda/data/ErrorResponse.kt
+++ data/src/main/java/org/tuvecinoteayuda/data/ErrorResponse.kt
@@ -4,5 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ErrorResponse(
     @field:SerializedName("code") var code: Int,
-    @field:SerializedName("message") var message: String
+    @field:SerializedName("status_code") var statusCode: Int,
+    @field:SerializedName("message") var message: String,
+    @field:SerializedName("status") var status: String
 )
