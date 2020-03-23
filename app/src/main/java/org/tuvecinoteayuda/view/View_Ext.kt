6d636d9783a0fe70dbb8ac20a55c05fba5a2e52a
Roberto <rmp0046@gmail.com>
2020-03-22 22:13:53 -0300

    control for phone and zip code

--- app/src/main/java/org/tuvecinoteayuda/view/View_Ext.kt
+++ app/src/main/java/org/tuvecinoteayuda/view/View_Ext.kt
@@ -1,7 +1,10 @@
 package org.tuvecinoteayuda.view
 
+import android.text.InputFilter
+import android.text.InputFilter.LengthFilter
 import android.view.View
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 fun View?.hide() {
@@ -16,6 +19,11 @@ fun View?.invisible() {
     this?.visibility = View.INVISIBLE
 }
 
-fun TextInputLayout.removeErrorOnTyping() {
-    this.editText?.addTextChangedListener { this.error = null }
+fun TextInputLayout?.removeErrorOnTyping() {
+    this?.editText?.addTextChangedListener { this.error = null }
 }
+
+fun TextInputEditText?.setMaxLenght(maxLength: Int) {
+    this?.filters = arrayOf<InputFilter>(LengthFilter(maxLength))
+}
+
