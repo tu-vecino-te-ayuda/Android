761e5e18de31504996119459d4688180700cb22d
David Miguel Lozano <me@davidmiguel.com>
2020-03-28 16:02:35 +0100

    Update help request repository

--- data/src/main/java/org/tuvecinoteayuda/data/helprequests/api/HelpRequestApi.kt
+++ data/src/main/java/org/tuvecinoteayuda/data/helprequests/api/HelpRequestApi.kt
@@ -1,31 +1,33 @@
 package org.tuvecinoteayuda.data.helprequests.api
 
 import org.tuvecinoteayuda.data.commons.models.MessageResponse
+import org.tuvecinoteayuda.data.helprequests.models.CreateHelpRequestRequest
 import org.tuvecinoteayuda.data.helprequests.models.HelpRequestListResponse
 import org.tuvecinoteayuda.data.helprequests.models.HelpRequestResponse
 import org.tuvecinoteayuda.data.helprequests.models.HelpRequestTypeResponse
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface HelpRequestApi {
-
-    @GET("help-requests/pending")
-    suspend fun pendingRequest(): HelpRequestResponse
-
-    @GET("help-requests")
-    suspend fun getRequest(): HelpRequestListResponse
 
     @GET("help-request-types")
     suspend fun getHelpRequestTypes(): HelpRequestTypeResponse
 
-    @GET("help-requests/accept/{id}")
-    suspend fun acceptRequest(@Path("id") id: String): HelpRequestListResponse
+    @GET("help-requests")
+    suspend fun getMyHelpRequests(): HelpRequestListResponse
 
-    @DELETE("help-requests/revert/{id}")
-    suspend fun cancelAcceptedRequest(@Path("id") id: String): MessageResponse
+    @POST("help-requests")
+    suspend fun createHelpRequest(@Body helpRequest: CreateHelpRequestRequest): MessageResponse
 
     @DELETE("help-requests/{id}")
-    suspend fun cancelMyRequest(@Path("id") id: String): MessageResponse
+    suspend fun deleteHelpRequest(@Path("id") id: String): MessageResponse
+
+    @GET("help-requests/pending")
+    suspend fun getPendingHelpRequests(): HelpRequestResponse
+
+    @GET("help-requests/accept/{id}")
+    suspend fun acceptHelpRequest(@Path("id") id: String): HelpRequestListResponse
+
+    @DELETE("help-requests/revert/{id}")
+    suspend fun cancelAcceptedHelpRequest(@Path("id") id: String): MessageResponse
 
 }
